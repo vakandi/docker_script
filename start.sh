6d990bd8f65a21383cef6c81e97f4598d23d905a
vakandi <wael.bousfira@protonmail.com>
2023-05-01 16:49:14 +0100

    update

--- start.sh
+++ start.sh
@@ -2,12 +2,11 @@
 systemctl stop apache2
 systemctl stop mariadb
 
-docker_path='/root/leetsec_hackathons/webpentest'
 mount_point='/mnt'
 
 docker pull debian:bullseye
 docker build -t debian:bullseye .
-docker run -p 80:80 -p 42:42 -it -d --name nft_store -v $docker_path:$mount_point/ debian:bullseye bash
+docker run -p 80:80 -p 42:42 -it -d --name nft_store -v $PWD:$mount_point/ debian:bullseye bash
 #docker run -p 80:80 -p 22:22 -it --name nft_store -v /root/hackathons-webpentest-challenge/hackaton-v2/docker:/mnt/ debian:bullseye bash
 echo " "
 echo "\033[42m DOCKER STARTED \033[0m"
